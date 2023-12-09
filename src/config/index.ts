fa911eeefbbd3c9228ad0044c01aba0a355c1f22
colinmyers37 <colinmyers37@gmail.com>
2023-12-09 02:20:03 -0600

    additional work

---
+++ src/config/index.ts
@@ -0,0 +1,34 @@
+export const PRODUCT_CATEGORIES = [
+  {
+    label: "Tracking",
+    value: "tracking" as const,
+    featured: [
+      {
+        name: "Inventory Analytics",
+        href: "#",
+        imageSrc: "/nav/ui-kits/mixed.jpg",
+      },
+      {
+        name: "Recommendations",
+        href: "#",
+        imageSrc: "/nav/ui-kits/mixed.jpg",
+      },
+    ],
+  },
+  {
+    label: "Armoury",
+    value: "armoury" as const,
+    featured: [
+      {
+        name: "All Firearms",
+        href: "#",
+        imageSrc: "/nav/ui-kits/mixed.jpg",
+      },
+      {
+        name: "Favorites",
+        href: "#",
+        imageSrc: "/nav/ui-kits/mixed.jpg",
+      },
+    ],
+  },
+];
