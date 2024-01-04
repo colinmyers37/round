93881dd94a40769056050fd86ac7f0f9d93a0256
colinmyers37 <colinmyers37@gmail.com>
2024-01-03 20:54:07 -0600

    get server setup and connect to db

---
+++ src/server/server.ts
@@ -0,0 +1,30 @@
+import { getPayloadClient } from "../util/get-payload";
+import { nextApp, nextHandler } from "../util/next-utils";
+import express from "express";
+
+const app = express();
+const PORT = Number(process.env.PORT) || 3000;
+
+const start = async () => {
+  const payload = await getPayloadClient({
+    initOptions: {
+      express: app,
+      onInit: async (cms) => {
+        cms.logger.info(`Admin URL ${cms.getAdminURL()}`);
+      },
+    },
+  });
+  app.use((req, res) => nextHandler(req, res));
+
+  nextApp.prepare().then(() => {
+    payload.logger.info("Next.js start");
+
+    app.listen(PORT, () => {
+      payload.logger.info(
+        `Next.js App URL: ${process.env.NEXT_PUBLIC_APP_URL}`
+      );
+    });
+  });
+};
+
+start();
