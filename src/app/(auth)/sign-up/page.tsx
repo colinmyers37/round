cff05a2935babed87fe4f5aa4f6cb9546c72c3a7
colinmyers37 <colinmyers37@gmail.com>
2024-01-10 02:55:53 -0600

    added signup form

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import Image from "next/image";
+import Logo from "../../../images/logoBackground.png";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  AuthCredentialsValidator,
+  TAuthCredentialsValidator,
+} from "@/lib/validators/account-credentials-validator";
+
+const Page = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TAuthCredentialsValidator>({
+    resolver: zodResolver(AuthCredentialsValidator),
+  });
+
+  const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
+    //send data to server
+  };
+
+  return (
+    <>
+      <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col items-center space-y-2 text-center">
+            <Image
+              src={Logo}
+              alt="Logo"
+              width={80}
+              height={80}
+              className="w-auto h-15"
+            />
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Create an account
+            </h1>
+
+            <Link
+              className={buttonVariants({
+                variant: "link",
+                className: "gap-1.5",
+              })}
+              href="/sign-in"
+            >
+              Already have an account? Sign in
+              <ArrowRight className="h-4 w-4" />
+            </Link>
+          </div>
+
+          <div className="grid gap-6">
+            <form onSubmit={onSubmit}>
+              <div className="grid gap-2">
+                <div className="grid gap-1 py-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    {...register("email")}
+                    className={cn({
+                      "focus-visible:ring-red-500": errors.email,
+                    })}
+                    placeholder="you@example.com"
+                  />
+                </div>
+                <div className="grid gap-1 py-2">
+                  <Label htmlFor="password">Password</Label>
+                  <Input
+                    {...register("password")}
+                    className={cn({
+                      "focus-visible:ring-red-500": errors.password,
+                    })}
+                    placeholder="Password"
+                  />
+                </div>
+
+                <Button>Sign up</Button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Page;
